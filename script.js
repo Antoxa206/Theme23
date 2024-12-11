f1f2847ff8a784f9f9a88dfce1949db5e3b6df7e
Anton Lobinskiy <lobinskiatnot@gmail.com>
2024-12-11 08:04:22 +0100

    complete

--- script.js
+++ script.js
@@ -1,60 +1,25 @@
-function processUserInput(callback) {
-    const name = prompt('Введіть ваше ім\'я:');
-    setTimeout(function () {
-      callback(name);
-    }, 3000); // Задержка 3 секунды перед вызовом коллбека
-  }
-  
-  function greet(name) {
-    console.log('Привіт, ' + name + '!');
-  }
-  
-  // Использование
-  processUserInput(greet);
-  
-  const car = {
-    model: 'Model X',
-    name: 'Tesla',
-    year: '2015',
-  
-    ShowDetails: function(){
-      setTimeout (() => {
-        console.log(`Назва: ${this.name}, Модель: ${this.model}, Год випуску: ${this.year}`);
-      }, 2000);
-    }
-  };
-  
-  car.ShowDetails();
-  
-  const add = (a, b) => a + b;
-  
-  console.log(add(3,4));
-  
-  function sum(...args) {
-    return args.reduce((total, num) => total + num, 0);
-  }
-  
-  console.log(sum(1, 2, 3, 4));
-  
-  const number = {
-    x: 42,
-    getX: function() {
-      return this.x;
-    }
-  };
-  
-  const retrieveX = number.getX;
-  console.log(retrieveX()); 
-  
-  const boundGetX = retrieveX.bind(number);
-  console.log(boundGetX());
-  
-  const person1 = { name: 'John' };
-  const person3 = { name: 'Jane' };
-  
-  function introduce() {
-    console.log(`Привіт, мене звуть ${this.name}`);
-  }
-  
-  introduce.call(person1);  
-  introduce.call(person3);
+const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+array.forEach(function(number) {
+    console.log(number);
+});
+
+const doubled = array.map(function(numbers) {
+    return numbers * 2;
+  });
+  console.log(doubled);
+
+  const newfilter = array.filter(function(number) {
+    return number > 3;
+  });
+  console.log(newfilter);
+
+  const sum = array.reduce(function(acc, number) {
+    return acc + number;
+  }, 0);
+  console.log(sum); 
+
+  const greaterfive = array.some(function(number) {
+    return number > 5;
+  });
+  console.log(greaterfive);
